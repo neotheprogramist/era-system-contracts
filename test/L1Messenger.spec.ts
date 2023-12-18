435abedf7b34fbeb65747e688bafb991fec1174b
Uacias <filgum0326@gmail.com>
2023-12-18 00:51:53 +0000

    simplified passing

--- test/L1Messenger.spec.ts
+++ test/L1Messenger.spec.ts
@@ -1,23 +1,29 @@
 import { ethers } from "hardhat";
-import type { L1Messenger } from "../typechain-types";
+import { L1Messenger__factory, type L1Messenger } from "../typechain-types";
 import { prepareEnvironment } from "./shared/mocks";
-import { deployContract } from "./shared/utils";
+import { deployContract, deployContractOnAddress, getWallets } from "./shared/utils";
+import { Wallet } from "zksync-web3";
+import { TEST_L1_MESSENGER_SYSTEM_CONTRACT_ADDRESS } from "./shared/constants";
+import { expect } from "chai";
 
 describe("L1Messenger tests", () => {
     let l1Messenger: L1Messenger;
+    let wallet: Wallet;
 
     before(async () => {
         await prepareEnvironment();
-        l1Messenger = (await deployContract("L1Messenger")) as L1Messenger;
+        wallet = getWallets()[0];
+        l1Messenger = L1Messenger__factory.connect(TEST_L1_MESSENGER_SYSTEM_CONTRACT_ADDRESS, wallet);
     });
 
     describe("sendL2ToL1Log", async () => {
-        it("should fail", async () => {
+        it("should probably pass", async () => {
             const isService = true;
             const key = ethers.utils.formatBytes32String("key");
             const value = ethers.utils.formatBytes32String("value");
-            await l1Messenger.sendL2ToL1Log(isService, key, value);
+
+            await expect(l1Messenger.connect(wallet).sendL2ToL1Log(isService, key, value))
+                .to.emit(l1Messenger, "L2ToL1LogSent");
         });
     });
-
 });
